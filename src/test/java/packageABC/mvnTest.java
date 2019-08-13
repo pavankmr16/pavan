5415b276cfced4a0591cad93f09df22afe6cc09a
pavan <pavankmr16@gmail.com>
2019-08-13 10:49:40 +0530

    This is my first commit

---
+++ src/test/java/packageABC/mvnTest.java
@@ -0,0 +1,20 @@
+package packageABC;
+
+import org.testng.annotations.Test;
+
+public class mvnTest 
+{
+	@Test
+	public void webapp()
+	{
+		System.out.println("welcome to mvn");
+	}
+	
+	@Test
+	public void mobileapp()
+	{
+		System.out.println("welcome to mobile ");
+	}
+	
+
+}
